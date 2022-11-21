76d23b64b84c29c036e095fe4c35851342d47afd
Yuriy <yura.kim-KIM@yandex.ru>
2022-11-21 22:05:33 +0300

    add authorization and tests

--- service/api/views.py
+++ service/api/views.py
@@ -1,11 +1,21 @@
+import os
 from typing import List
 
-from fastapi import APIRouter, FastAPI, Request
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, FastAPI, Request, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security.api_key import APIKey
 from pydantic import BaseModel
 
-from service.api.exceptions import UserNotFoundError, ModelNotFoundError
-from service.log import app_logger
 from reco_models.model_validator import ModelValidator
+from service.api.exceptions import (
+    ModelNotFoundError,
+    NotAuthorizedError,
+    UserNotFoundError,
+)
+from service.log import app_logger
+
+load_dotenv()
 
 
 class RecoResponse(BaseModel):
@@ -15,6 +25,18 @@ class RecoResponse(BaseModel):
 
 router = APIRouter()
 validator = ModelValidator()
+
+security_scheme = HTTPBearer()
+API_KEY = os.getenv("API_KEY")
+# print(API_KEY)
+
+
+async def check_key(
+    key: HTTPAuthorizationCredentials = Security(security_scheme),
+):
+    if key.credentials == API_KEY:
+        return key.credentials
+    raise NotAuthorizedError()
 
 
 @router.get(
@@ -29,11 +51,14 @@ async def health() -> str:
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
     response_model=RecoResponse,
+    responses={404: {"description": "User/model not found"},
+               401: {"description": "Authorization error"}},
 )
 async def get_reco(
     request: Request,
     model_name: str,
     user_id: int,
+    api_key: APIKey = Depends(check_key)
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
@@ -45,10 +70,6 @@ async def get_reco(
         raise ModelNotFoundError(error_message=f"Model {model_name} not found")
     else:
         reco = model.recommend(user_id)
-    
-    # k_recs = request.app.state.k_recs
-    print(reco)
-    # reco = list(range(k_recs))
     return RecoResponse(user_id=user_id, items=reco)
 
 
