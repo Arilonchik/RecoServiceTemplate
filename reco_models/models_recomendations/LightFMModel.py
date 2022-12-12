2265edc1af495253588663f3055c0141680fd138
Yuriy <yura.kim-KIM@yandex.ru>
2022-12-12 18:42:19 +0300

    fix lightfm

--- reco_models/models_recomendations/LightFMModel.py
+++ reco_models/models_recomendations/LightFMModel.py
@@ -6,9 +6,10 @@ class LightFMModel(BaseRecoModel):
     def __init__(self):
         super().__init__()
         self.offline_model = pickle.load(open("./reco_models/models_raw/offline_lightfm.pkl", 'rb'))
+        self.popular = pickle.load(open("./reco_models/models_raw/popular.pkl", 'rb'))
         
     def recommend(self, user_id):
         try:
             return self.offline_model[user_id]
         except:
-            return [10440, 15297, 9728, 13865, 4151, 3734, 2657, 4880, 142, 6809]
+            return self.popular
